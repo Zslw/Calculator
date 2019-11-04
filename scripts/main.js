d66161e1c5007147ea58ea809a4a2cad5a96d56d
Zslw <dan.juarez192000@gmail.com>
2019-11-04 13:38:44 -0600

    added buttons that enter values and perfom some calculations

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,113 @@
+let mainDisplay = '0';
+let secondaryDisplay = '0';
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button, mainDisplay) => button.addEventListener('click', buttonEvent));
+
+function buttonEvent(e) {
+    processEvent(e.target.innerText);
+};
+
+function processEvent(value) {//This is the main functiion.
+    const md = document.querySelector('#mainDisplay');
+    const sd = document.querySelector('#secondaryDisplay');
+
+    let operatorArray = mainDisplay.split(" ");
+    //  trailing space was added. 
+    if (operatorArray[operatorArray.length-1] === "") operatorArray.splice(operatorArray.length-1,1);
+    let lastMd = operatorArray[operatorArray.length-1] 
+    console.log(lastMd)
+
+
+    // Loop to run through the various inputs that couuld be made.  Could be re-factored at a later date.
+    if (value === "c" ) {
+        mainDisplay="0";
+        md.classList.add("newNumber");
+        secondaryDisplay = "";
+        sd.innerText = secondaryDisplay;
+    }else if (value === "BS") {
+        if (mainDisplay.length === 1 || md.classList.contains("newNumber")) {
+            mainDisplay="0";
+        } else {
+            if (lastMd === " ") mainDisplay=mainDisplay.slice(0, -1);
+            mainDisplay=mainDisplay.slice(0, -1);
+        }
+    }else if (!isNaN(value)){
+        if (mainDisplay === "0"  || md.classList.contains("newNumber")) {
+            md.classList.remove("newNumber");
+            mainDisplay = value;
+        }else { 
+            mainDisplay+=value;
+        }
+    }else if (value === ".") {
+        if (!operatorArray[operatorArray.length-1].includes(".")) {
+            mainDisplay+=value;
+            md.classList.remove("newNumber");
+        }
+    }else if (value !== "=" && !isNaN(lastMd)){
+        mainDisplay = mainDisplay+ " " + value + " ";
+        md.classList.remove("newNumber");
+    } else if (value === "=") {
+            md.classList.add("newNumber");
+            operate();
+    }
+
+    md.innerText = mainDisplay;
+}
+
 function add(a,b){
-    return s = a+b;
+    return Number(a)+Number(b);
 }
 function subtract(a,b){
-    return res = a-b;
+    return Number(a)-Number(b);
 }
 function multiply(a,b){
-    return mul = a*b;
+    return Number(a)*Number(b);
 }
 function divide(a,b){
-    return divv = a/b;
+    return round(Number(a)/Number(b),4);
 }
-function operate(number1,operator,number2){
+
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function operate(){
+    const md = document.querySelector('#mainDisplay');
+    const sd = document.querySelector('#secondaryDisplay');
+    sd.innerText = mainDisplay;
+    let operatorArray = mainDisplay.split(" ");
+
+    if (operatorArray.length%2 === 0){
+        console.log("remove trailing operator");
+        operatorArray.splice(operatorArray.length-1,1);
+    }
+
+    let number1 = operatorArray[0];
+    let operator = operatorArray[1];
+    let number2 = operatorArray[2];
+    let output = 0;
     switch (operator){
-        case 'plus':
-            return add(number1,number2);
+        case '+':
+            output = add(number1,number2);
             break;
-        case 'minus':
-            return subtract(number1,number2);
+        case '-':
+            output = subtract(number1,number2);
             break;
-        case 'times':
-            return multiply(number1,number2);
+        case '×':
+            output = multiply(number1,number2);
             break;
-        case 'divided by':
-            return divide(number1,number2);
+        case '÷':
+                if (number2 == 0) {
+                    sd.innerText = "Error Can't Divide by 0";
+                } 
+            output = divide(number1,number2);
             break;
         default:
-            prompt('Enter a basic opperator')
+            alert('Enter a basic opperator');
     }
+
+    mainDisplay = ""+ output; //Make sure that this casts to string
+    md.innerText = mainDisplay;
+    sd.innerText = sd.innerText + " = " + output;
+
 }
